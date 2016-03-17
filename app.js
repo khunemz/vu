482bd0d4a593ad155995dc6cf1e957a0e4389183
khunemz <khunemz@gmail.com>
2016-03-17 14:53:31 +0700

    Basic CRUD To-do app complete with Vue .js

--- app.js
+++ app.js
@@ -2,7 +2,38 @@ new Vue({
 
 	el: '#tasks',
 
+	//model
 	data: {
-		heading: 'Hello'
+		tasks: [{ body: 'Yep', completed: false}],
+		newTask: []
+	},
+
+	//method
+	methods: {
+		addTask: function (e) {
+			e.preventDefault();
+			this.tasks.push({
+				body: this.newTask,
+				completed: false
+			});
+
+			this.newTask='';
+		},
+
+		editTask: function(task){
+			//remove task
+			this.removeTask(task);
+			//update newTask input
+			this.newTask = task.body;
+			//focus the newTask input
+			//the $$ is pointing to newTask DOM node
+			this.$$.newTask.focus();
+		},
+
+		
+		removeTask: function(task){
+			this.tasks.$remove(task);
+		}
+
 	}
 })
